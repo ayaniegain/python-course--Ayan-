4c4ed8ead66710510374f4efd10c133190ddbc0e
ayaniegain <ayaniegain@gmail.com>
2024-12-17 11:42:11 +0530

    python first one hour note

---
+++ assignment/program1.py
@@ -0,0 +1,28 @@
+# 1
+# redius=int(input("giveen input"))
+
+# area=int(3.37*redius*redius)
+
+# print("area is :",area)
+
+# 2
+# Celsius = (Fahrenheit - 32) * 5/9.
+
+# Fahrenheit=int(input("giveen input Tem in F:"))
+
+# Celsius =(Fahrenheit - 32) * 5/9.
+
+# print(Celsius)
+
+# Assignment 3: Variable Swap
+
+a=20
+b=30
+a=(b+a)-a #30
+a=b
+b=(a+b)-b
+# b=a #b=20
+# b=(b+a)-b
+
+print("a is",a)
+print("b is",b)
---
+++ course/01_start.py
@@ -0,0 +1,87 @@
+#--------starter--------
+# age= 30;
+# AGE=99;
+# Age=99
+# name="ayan"
+# print(AGE)
+# no=4
+# no=7
+# print(no+no)
+
+# print(int(2))
+
+# -------Input-----
+
+# user_input_one=input("Enter first value:")
+# user_input_two=input("Enter second value:")
+
+# print(user_input_one,user_input_two)  
+
+# ---operator-----
+# x=70
+# x+=4
+# print(x)
+# ---------String-------
+
+# str= "Ayan Biaswas"
+
+# multi='''
+#     hello
+#     hi
+#     how are you?
+# '''
+
+# print(multi)
+# //length-------
+# print(len("hello"))
+
+# string index and slicing---------
+
+# text="Python";
+
+# first_char=text[0]
+# sliced_char=text[1:3]
+
+# print(sliced_char) ##yt
+
+# formated string--------
+
+# name="Ayan"
+# age=26
+
+# fortated_str=f"hello i am {name} i am {age} year's old"
+
+# print(fortated_str)
+
+# print(f'sum of "2" + "2" is {2+2}')
+
+# Escape characters------------
+
+# \n and \t 
+
+
+# formated string -------
+
+# age =30
+# age2=40
+
+# frt_age="new age is {} {}".format(age,age2)
+
+# %s,%d,%f
+
+# frt_age="new age is %d %d" %(age,age2)
+
+
+# print(frt_age)
+
+#  TYPE and Type casting 
+
+# print(type ("str"))
+# print(type (123))
+# print(type (True))
+# print(type ([4,2,3]))
+
+# typecasting-----
+# print( int(34.6))
+# print( float(34))
+# print( bool(0))
